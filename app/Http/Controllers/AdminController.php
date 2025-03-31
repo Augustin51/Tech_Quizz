0b514293430c9f38f20f69f1e54fee8ba08702f1
MaitregGims <tcreusilletevrard@edenschool.fr>
2025-03-31 11:24:00 +0200

    supression des urilisateurs possible

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,14 +7,28 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
+
 {
-    public function index()
+    public function checkAdmin()
     {
-        if (Auth::user()->role === 1) {
-            $users = User::all();
-            return view("admin", ["data" => $users]);
-        } else {
+        if (Auth::user()->role != 1) {
             abort(403, 'Accès interdit. Réservé aux administrateurs.');
         }
+    }
+    public function index()
+    {
+        $this->checkAdmin();
+
+        $users = User::all();
+        return view("admin", ["data" => $users]);
+    }
+
+    public function destroy($id)
+    {
+        $this->checkAdmin();
+
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect("/admin");
     }
 }
